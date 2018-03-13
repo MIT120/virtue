17e7300a24b4ecbd19c2f51ebf0de32cb7c5438a
Unknown <mataharata@gmail.com>
2018-03-13 18:51:16 +0100

    Models merge

--- gatherer/models.py
+++ gatherer/models.py
@@ -15,29 +15,6 @@ ENUMCHOICES = (
     ('y', 'On'),
     ('n', 'Off'),
 )
-class Appliance(models.Model):
-    flat_id = models.ForeignKey(Flat, on_delete=models.CASCADE, primary_key=True, max_length=45)
-    room_id = models.ForeignKey(Room, on_delete=models.CASCADE, primary_key=True, max_length=45)
-    appliance_id = models.CharField(primary_key=True, max_length=45)
-    appliance_type_id = models.ForeignKey(List_Of_All_Appliance_in_building, on_delete=models.CASCADE, primary_key=True)
-    appliance_status = models.CharField(max_length=1, choices=ENUMCHOICES)
-    last_appliance_energy_consumed = models.FloatField(null=True)
-    last_reading_time = models.DateField()
-
-
-class List_Of_All_Appliance_in_building(models.Model):
-    appliance_type_id = models.AutoField(primary_key=True)
-    appliance_name = models.CharField(max_length=45)
-    appliance_power = models.IntegerField()
-
-
-class Appliance_Readings(models.Model):
-    reading_time = models.DateField(primary_key=True)
-    flat_id = models.ForeignKey(Flat, on_delete=models.CASCADE, max_length=45)
-    room_id = models.ForeignKey(Room, on_delete=models.CASCADE, max_length=45)
-    appliance_id = models.ForeignKey(Appliance, on_delete=models.CASCADE, max_length=45)
-    appliance_type_id = models.ForeignKey(List_Of_All_Appliance_in_building, on_delete=models.CASCADE)
-    appliance_energy_consumed = models.FloatField(null=True)
 
 class Building(models.Model):
   building_id = models.IntegerField(primary_key=True)
@@ -49,22 +26,14 @@ class Building(models.Model):
   building_name = models.CharField(max_length=45)
 
 class Flat(models.Model):
-  flat_id = models.CharField(max_length=45, primary_key=True)
-  floor_nr = models.IntegerField()
-  time_created = models.DateField()
-  time_updated = models.DateField()
-  type = models.CharField(max_length=45)
-  nr_of_people = models.IntegerField()
-  building_id = models.ForeignKey(Building, on_delete=models.CASCADE)
-  flat_rating = models.FloatField(null=True)
-
-class Room_Readings(models.Model):
-  reading_time = models.DateField(primary_key=True)
-  flat_id = models.ForeignKey(Flat, on_delete=models.CASCADE)
-  room_id = models.ForeignKey(Room, on_delete=models.CASCADE)
-  temperature = models.FloatField(null=True)
-  humidity = models.FloatField(null=True)
-  amount_of_CO2 = models.FloatField(null=True)
+    flat_id = models.CharField(max_length=45, primary_key=True)
+    floor_nr = models.IntegerField()
+    time_created = models.DateField()
+    time_updated = models.DateField()
+    type = models.CharField(max_length=45)
+    nr_of_people = models.IntegerField()
+    building_id = models.ForeignKey(Building, on_delete=models.CASCADE)
+    flat_rating = models.FloatField(null=True)
 
 class Room(models.Model):
   flat_id = models.ForeignKey(Flat, on_delete=models.CASCADE, primary_key=True)
@@ -76,6 +45,14 @@ class Room(models.Model):
   last_reading_time = models.DateField()
   nr_of_appliances = models.IntegerField()
 
+class Room_Readings(models.Model):
+  reading_time = models.DateField(primary_key=True)
+  flat_id = models.ForeignKey(Flat, on_delete=models.CASCADE)
+  room_id = models.ForeignKey(Room, on_delete=models.CASCADE)
+  temperature = models.FloatField(null=True)
+  humidity = models.FloatField(null=True)
+  amount_of_CO2 = models.FloatField(null=True)
+
 class Weather(models.Model):
   weather_id = models.AutoField(primary_key=True)
   building_id = models.ForeignKey(Building, on_delete=models.CASCADE)
@@ -86,3 +63,26 @@ class Weather(models.Model):
   wind_direction = models.IntegerField()
   solar_rediation = models.FloatField(null=True)
   amount_of_CO2 = models.FloatField(null=True)
+
+class List_Of_All_Appliance_in_building(models.Model):
+    appliance_type_id = models.AutoField(primary_key=True)
+    appliance_name = models.CharField(max_length=45)
+    appliance_power = models.IntegerField()
+
+class Appliance(models.Model):
+    flat_id = models.ForeignKey(Flat, on_delete=models.CASCADE, primary_key=True, max_length=45)
+    room_id = models.ForeignKey(Room, on_delete=models.CASCADE, primary_key=True, max_length=45)
+    appliance_id = models.CharField(primary_key=True, max_length=45)
+    appliance_type_id = models.ForeignKey(List_Of_All_Appliance_in_building, on_delete=models.CASCADE, primary_key=True)
+    appliance_status = models.CharField(max_length=1, choices=ENUMCHOICES)
+    last_appliance_energy_consumed = models.FloatField(null=True)
+    last_reading_time = models.DateField()
+
+
+class Appliance_Readings(models.Model):
+    reading_time = models.DateField(primary_key=True)
+    flat_id = models.ForeignKey(Flat, on_delete=models.CASCADE, max_length=45)
+    room_id = models.ForeignKey(Room, on_delete=models.CASCADE, max_length=45)
+    appliance_id = models.ForeignKey(Appliance, on_delete=models.CASCADE, max_length=45)
+    appliance_type_id = models.ForeignKey(List_Of_All_Appliance_in_building, on_delete=models.CASCADE)
+    appliance_energy_consumed = models.FloatField(null=True)
