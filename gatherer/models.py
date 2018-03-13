c7476403311d8ba257fa249434c91b519ef15734
Unknown <mataharata@gmail.com>
2018-03-13 18:43:06 +0100

    fix on ForeignKeys

--- gatherer/models.py
+++ gatherer/models.py
@@ -16,23 +16,25 @@ ENUMCHOICES = (
     ('n', 'Off'),
 )
 class Appliance(models.Model):
-    flat_id = models.Foreign_key(Flat, on_delete=models.CASCADE, primary_key=True, max_length=45)
-    room_id = models.Foreign_key(Room, on_delete=models.CASCADE, primary_key=True, max_length=45)
+    flat_id = models.ForeignKey(Flat, on_delete=models.CASCADE, primary_key=True, max_length=45)
+    room_id = models.ForeignKey(Room, on_delete=models.CASCADE, primary_key=True, max_length=45)
     appliance_id = models.CharField(primary_key=True, max_length=45)
-    appliance_type_id = models.Foreign_key(List_Of_All_Appliance_in_building, on_delete=models.CASCADE, primary_key=True)
+    appliance_type_id = models.ForeignKey(List_Of_All_Appliance_in_building, on_delete=models.CASCADE, primary_key=True)
     appliance_status = models.CharField(max_length=1, choices=ENUMCHOICES)
     last_appliance_energy_consumed = models.FloatField(null=True)
     last_reading_time = models.DateField()
+
+
 class List_Of_All_Appliance_in_building(models.Model):
     appliance_type_id = models.AutoField(primary_key=True)
     appliance_name = models.CharField(max_length=45)
     appliance_power = models.IntegerField()
+
+
 class Appliance_Readings(models.Model):
     reading_time = models.DateField(primary_key=True)
-    flat_id = models.Foreign_key(Flat, on_delete=models.CASCADE, max_length=45)
-    room_id = models.Foreign_key(Room, on_delete=models.CASCADE, max_length=45)
-    appliance_id = models.Foreign_key(Appliance, on_delete=models.CASCADE, max_length=45)
-    appliance_type_id = models.Foreign_key(List_Of_All_Appliance_in_building, on_delete=models.CASCADE)
+    flat_id = models.ForeignKey(Flat, on_delete=models.CASCADE, max_length=45)
+    room_id = models.ForeignKey(Room, on_delete=models.CASCADE, max_length=45)
+    appliance_id = models.ForeignKey(Appliance, on_delete=models.CASCADE, max_length=45)
+    appliance_type_id = models.ForeignKey(List_Of_All_Appliance_in_building, on_delete=models.CASCADE)
     appliance_energy_consumed = models.FloatField(null=True)
-
-
