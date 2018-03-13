12856a7af5751cbc9eb665b9b68e5079eaa2a8e7
Unknown <mataharata@gmail.com>
2018-03-13 18:45:53 +0100

    Rali models

--- gatherer/models.py
+++ gatherer/models.py
@@ -38,3 +38,51 @@ class Appliance_Readings(models.Model):
     appliance_id = models.ForeignKey(Appliance, on_delete=models.CASCADE, max_length=45)
     appliance_type_id = models.ForeignKey(List_Of_All_Appliance_in_building, on_delete=models.CASCADE)
     appliance_energy_consumed = models.FloatField(null=True)
+
+class Building(models.Model):
+  building_id = models.IntegerField(primary_key=True)
+  city = models.CharField(max_length=50)
+  street = models.CharField(max_length=45)
+  postcode = models.CharField(max_length=45)
+  country = models.CharField(max_length=45)
+  nr_Of_Floors = models.IntegerField()
+  building_name = models.CharField(max_length=45)
+
+class Flat(models.Model):
+  flat_id = models.CharField(max_length=45, primary_key=True)
+  floor_nr = models.IntegerField()
+  time_created = models.DateField()
+  time_updated = models.DateField()
+  type = models.CharField(max_length=45)
+  nr_of_people = models.IntegerField()
+  building_id = models.ForeignKey(Building, on_delete=models.CASCADE)
+  flat_rating = models.FloatField(null=True)
+
+class Room_Readings(models.Model):
+  reading_time = models.DateField(primary_key=True)
+  flat_id = models.ForeignKey(Flat, on_delete=models.CASCADE)
+  room_id = models.ForeignKey(Room, on_delete=models.CASCADE)
+  temperature = models.FloatField(null=True)
+  humidity = models.FloatField(null=True)
+  amount_of_CO2 = models.FloatField(null=True)
+
+class Room(models.Model):
+  flat_id = models.ForeignKey(Flat, on_delete=models.CASCADE, primary_key=True)
+  room_id = models.CharField(max_length=45, primary_key=True)
+  room_name = models.CharField(max_length=45)
+  last_humidity = models.FloatField(null=True)
+  last_temperature = models.FloatField(null=True)
+  last_amount_CO2 = models.FloatField(null=True)
+  last_reading_time = models.DateField()
+  nr_of_appliances = models.IntegerField()
+
+class Weather(models.Model):
+  weather_id = models.AutoField(primary_key=True)
+  building_id = models.ForeignKey(Building, on_delete=models.CASCADE)
+  reading_time = models.DateField()
+  temperature = models.FloatField(null=True)
+  humidity = models.FloatField(null=True)
+  windspeed = models.IntegerField()
+  wind_direction = models.IntegerField()
+  solar_rediation = models.FloatField(null=True)
+  amount_of_CO2 = models.FloatField(null=True)
